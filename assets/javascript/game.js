f55cb9ed7bb44103466ac2fc6c838a8b36acc83e
Brandon Williams <brando5393@gmail.com>
2019-12-24 11:20:56 -0600

    removed unused code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,42 +37,6 @@ var winCount = 0;
 $(document).ready(function () {
 
 
-    // var winCount = 0;
-    // var lossCount = 0;
-    // var userScore = 0;
-    // // computer needs to generate a random number between 19 and 120 that is then assigned to a var
-    // var computerNumber = Math.floor(Math.random() * 102) + 19;
-    // // buttons on the page should be given a value between 1 and 12
-    // var buttonValue = Math.floor(Math.random() * 12) + 1;
-
-
-    // // value vars
-    // var computerNumberText = $("#computer-number-display");
-    // var lossCountText = $("#loss-count-display");
-    // var winCountText = $("#win-count-display");
-    // var userScoreText = $("#user-score-display");
-
-    // // code to update game values
-    // computerNumberText.text(computerNumber);
-    // lossCountText.text(lossCount);
-    // winCountText.text(winCount);
-    // userScoreText.text(userScore);
-
-    // // reset game function
-    // // when user wins or looses the user's score resets, the wins or losses counter increases, computer selects a new random number and the buttons get new values
-    // function resetGame() {
-    //     computerNumber = Math.floor(Math.random() * 102) + 19;
-    //     buttonValue = Math.floor(Math.random() * 12) + 1;
-    //     computerNumberText.text(computerNumber);
-    //     lossCountText.text(lossCount);
-    //     winCountText.text(winCount);
-    //     userScore = 0;
-    //     userScoreText.text(userScore);
-        
-
-    // }
-
-
 
         // each of the 4 buttons on the page should be given a value between 1 and 12
         
